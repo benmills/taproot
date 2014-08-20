7372e7990838fc8b3876cc35fcb84d35035ec60f
benmills <ben@benmills.org>
2014-08-19 17:45:41 -0700

    tmp

--- app.rb
+++ app.rb
@@ -4,10 +4,17 @@ require "json"
 require "base64"
 require "pry"
 
+# Mark's test prod aaccount
+# Braintree::Configuration.environment = :production
+# Braintree::Configuration.merchant_id = 'nxtmb3rqt8tr35v2'
+# Braintree::Configuration.public_key = 'b2kqymfxnhx7wdtz'
+# Braintree::Configuration.private_key = '8f00a7a5df9541ec05f2fa8e1b333f29'
+
+# Ben's test prod account
 Braintree::Configuration.environment = :production
-Braintree::Configuration.merchant_id = 'nxtmb3rqt8tr35v2'
-Braintree::Configuration.public_key = 'b2kqymfxnhx7wdtz'
-Braintree::Configuration.private_key = '8f00a7a5df9541ec05f2fa8e1b333f29'
+Braintree::Configuration.merchant_id = 'dnnsn36rs57sqvym'
+Braintree::Configuration.public_key = 'pkcp77fk7m52gx3t'
+Braintree::Configuration.private_key = '9a37eb11d451554bfeece5f20ec1cf35'
 
 # Braintree::Configuration.environment = :sandbox
 # Braintree::Configuration.merchant_id = 'qh5bmbx4v6pzw93h'
@@ -38,11 +45,13 @@ get "/custom" do
 end
 
 post "/transaction" do
-  result = Braintree::Customer.create(:payment_method_nonce => params[:payment_method_nonce])
-  # token = result.customer.paypal_accounts.first.token
-  # result = Braintree::Transaction.sale(:amount => 1, :payment_method_token => token)
+  # result = Braintree::Customer.create(:payment_method_nonce => params[:payment_method_nonce])
+  # # token = result.customer.paypal_accounts.first.token
+  # # result = Braintree::Transaction.sale(:amount => 1, :payment_method_token => token)
 
-  binding.pry
+  # binding.pry
+
+  p params
 
   content_type :text
   params.inspect
