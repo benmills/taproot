bd5ee53cf2a6166c291b029d9fb1ee9cc8249bc2
benmills <ben@benmills.org>
2014-08-27 17:51:47 -0700

    Updates

--- app.rb
+++ app.rb
@@ -5,16 +5,22 @@ require "base64"
 require "pry"
 
 # Mark's test prod aaccount
+Braintree::Configuration.environment = :production
+Braintree::Configuration.merchant_id = 'nxtmb3rqt8tr35v2'
+Braintree::Configuration.public_key = 'b2kqymfxnhx7wdtz'
+Braintree::Configuration.private_key = '8f00a7a5df9541ec05f2fa8e1b333f29'
+
+# Mark's test prod aaccount
 # Braintree::Configuration.environment = :production
-# Braintree::Configuration.merchant_id = 'nxtmb3rqt8tr35v2'
-# Braintree::Configuration.public_key = 'b2kqymfxnhx7wdtz'
-# Braintree::Configuration.private_key = '8f00a7a5df9541ec05f2fa8e1b333f29'
+# Braintree::Configuration.merchant_id = '8ghb7q247srxc8cf'
+# Braintree::Configuration.public_key = '9n7p3vh9pfg8zvh2'
+# Braintree::Configuration.private_key = '8cec496e161abeae7a96d8ed2625c8f0'
 
 # Ben's test prod account
-Braintree::Configuration.environment = :production
-Braintree::Configuration.merchant_id = 'dnnsn36rs57sqvym'
-Braintree::Configuration.public_key = 'pkcp77fk7m52gx3t'
-Braintree::Configuration.private_key = '9a37eb11d451554bfeece5f20ec1cf35'
+# Braintree::Configuration.environment = :production
+# Braintree::Configuration.merchant_id = 'dnnsn36rs57sqvym'
+# Braintree::Configuration.public_key = 'pkcp77fk7m52gx3t'
+# Braintree::Configuration.private_key = '9a37eb11d451554bfeece5f20ec1cf35'
 
 # Braintree::Configuration.environment = :sandbox
 # Braintree::Configuration.merchant_id = 'qh5bmbx4v6pzw93h'
@@ -46,6 +52,13 @@ end
 get "/client_token/inspect" do
   content_type :json
   JSON.pretty_generate(JSON.parse(Base64.decode64(generate_client_token)))
+end
+
+get "/config/current" do
+  content_type :json
+  {
+    :merchant_id => Braintree::Configuration.merchant_id
+  }.to_json
 end
 
 get "/dropin" do
