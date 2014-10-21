7321ddd59064685cf8172c0eb0e49094472f081d
benmills <ben@benmills.org>
2014-10-21 14:10:06 -0700

    Add port

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-MERCHANT_ID=qh5bmbx4v6pzw93h PRIVATE_KEY=d3af2d01d2828a068eca39455f864d1e PUBLIC_KEY=4fmd3mrwgc9bdvkh MERCHANT_ACCOUNT=default ENVIRONMENT=sandbox ruby app.rb
+MERCHANT_ID=qh5bmbx4v6pzw93h PRIVATE_KEY=d3af2d01d2828a068eca39455f864d1e PUBLIC_KEY=4fmd3mrwgc9bdvkh MERCHANT_ACCOUNT=default ENVIRONMENT=sandbox ruby app.rb -p $PORT
